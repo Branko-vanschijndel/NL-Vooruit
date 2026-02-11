058312a629380334bc541fe1c543fea8d18e4618
Branko-vanschijndel <n401716@gymnasiumnovum.nl>
2026-02-11 12:48:10 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+// Highlight menu on scroll (alleen voor pagina's waar meerdere secties zijn)
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 150;
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href").includes(current)) {
+      link.classList.add("active");
+    }
+  });
+});
